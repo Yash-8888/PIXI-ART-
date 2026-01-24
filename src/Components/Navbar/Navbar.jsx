36845e55dd3168f2d974f30e557d596b608bbd3d
Yash-8888 <codingwithcandy@gmail.com>
2026-01-24 20:47:04 +0530

    componentes created

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <nav className="Navbar">
+        <img src="" alt="" />
+        <h3>PIXI-ART</h3>
+    </nav>
+  )
+}
+
+export default Navbar
